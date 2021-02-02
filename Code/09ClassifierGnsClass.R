6328cc6bf0835e68a1b6c07962427d85e09ed35f
Mohamed Omar <mohamed222ahmed222@gmail.com>
2021-02-02 17:49:14 -0500

    Look at the pairs

--- Code/09ClassifierGnsClass.R
+++ Code/09ClassifierGnsClass.R
@@ -51,3 +51,27 @@ table(PDLIM4_GSTP1_Class$GoodGene, PDLIM4_GSTP1_Class$Type)
 # GSTP1 < emt (anti-invasion) and growth (anti-growth)
 # TGFB2 < apoptosis (pro-apoptotic)
 
+
+
+###############
+## Which mechanism ??
+SKI_PDLIM4 <- allTSPs[which(allTSPs[,2] == "SKI" & allTSPs[,1] == "PDLIM4"), ]
+SKI_PDLIM4  #TAGs : SKI bad ! (oncogene) & PDLIM4 good ! (tumor suppressor)
+
+STK11_INO80 <- allTSPs[which(allTSPs[,1] == "STK11" & allTSPs[,2] == "INO80"), ]
+STK11_INO80 # Growth : STK11 good & INO80 Bad
+
+SETMAR_RAB11FIP4 <- allTSPs[which(allTSPs[,1] == "SETMAR" & allTSPs[,2] == "RAB11FIP4"), ]
+SETMAR_RAB11FIP4 # Growth: SETMAR: Good & RAB11FIP4 bad
+
+SATB1_TMEM176B <- allTSPs[which(allTSPs[,1] == "SATB1" & allTSPs[,2] == "TMEM176B"), ]
+SATB1_TMEM176B # Immune : SATB1: Good & TMEM: Bad
+
+PAPPA_TGFB2 <- allTSPs[which(allTSPs[,1] == "PAPPA" & allTSPs[,2] == "TGFB2"), ]
+PAPPA_TGFB2 # EMT: PAPPA: Good & TGFB2: Bad
+
+
+CTSB_GSTP1 <- allTSPs[which(allTSPs[,2] == "CTSB" & allTSPs[,1] == "GSTP1"), ]
+CTSB_GSTP1 # EMT: CTSB : Bad !! & GSTP1 : Good !!
+
+
